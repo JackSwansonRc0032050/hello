205a4bd288ed02aac856e8603dbb05dda970ebf5
jack swanson <RC0032050@CRO-24-04>
2018-09-10 14:48:29 +0100

    added a button

--- hello/Form1.Designer.cs
+++ hello/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(384, 168);
+            this.button1.Location = new System.Drawing.Point(400, 176);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(74, 26);
             this.button1.TabIndex = 0;
